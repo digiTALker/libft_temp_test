4238e3333efa01b44b3b1a181a51dba57be33da4
digiTALker <d_talker@mail.ru>
2020-05-10 22:54:09 +0300

    Update ft_memchr_.c

--- ft_memchr_.c
+++ ft_memchr_.c
@@ -2,12 +2,16 @@
 
 void  *ft_memchr(const void *s, int c, size_t n)
 {	
+	size_t i;
+
+	i = 0;
 	while(n)
 	{
-		if (*((unsigned char *)s+n) == ((unsigned char)c))
+		if (*((unsigned char *)s+i) == ((unsigned char)c))
 		{
-			return ((unsigned char *)s+n);
+			return ((unsigned char *)s+i);
 		}
+		i++;
 		n--;
 	}
 	return (NULL);
